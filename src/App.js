9bc8ace22f768a6fd3ed08cb1528bf1e49b2f868
Adaji Mukhtar <alfadaji@gmail.com>
2022-05-27 12:05:14 +0100

    completed the todo app

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
-import React, { useState } from 'react'
-import './App.css';
-import Form from './components/Form';
-import Header from './components/Header';
-import TodoList from './components/TodoList';
+import React, { useState } from "react";
+import "./App.css";
+import Form from "./components/Form";
+import Header from "./components/Header";
+import TodoList from "./components/TodoList";
 
-const  App = () => {
-  const [input, setInput] = useState('')
-    const [todos, setTodos] = useState([])
+const App = () => {
+  
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [editTodo, setEditTodo] = useState(null);
   return (
     <div className="container">
-      <div className='app-wrapper'>
+      <div className="app-wrapper">
         <div>
           <Header />
         </div>
         <div>
-          <Form input={input} setInput={setInput} todos={todos} setTodos={setTodos} />
+          <Form
+            input={input}
+            setInput={setInput}
+            todos={todos}
+            setTodos={setTodos}
+            editTodo={editTodo}
+            setEditTodo={setEditTodo}
+          />
         </div>
         <div>
-          <TodoList todos={todos} setTodos={setTodos} />
+          <TodoList
+            todos={todos}
+            setTodos={setTodos}
+            editTodo={editTodo}
+            setEditTodo={setEditTodo}
+          />
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,20 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 
-const Form = ({ input, setInput, todos, setTodos }) => {
+const Form = ({ input, setInput, todos, setTodos, editTodo, setEditTodo }) => {
+
+
+  const updateTodo = (title, id, completed) => {
+    const newTodo = todos.map((todo) => 
+      todo.id === id ? { title, id, completed } : todo
+    )
+    setTodos(newTodo)
+    setEditTodo('')
+  }
+
+  useEffect(() => {
+    if(editTodo) {
+      setInput(editTodo.title)
+    }else {
+      setInput('')
+    }
+  }, [setInput, editTodo])
     const onInputChange = (e) => {
         setInput(e.target.value)
     }
+
     const handleSubmit = (e) => {
         e.preventDefault()
-        setTodos([...todos, {id:uuidv4(), title: input, completed: false}, ])
+        if(!editTodo) {
+          setTodos([...todos, {id:uuidv4(), title: input, completed: false}, ])
         setInput('')
+        }else {
+          updateTodo(input, editTodo.id, editTodo.completed)
+        }
     }
   return (
     <form onSubmit={handleSubmit}>
       <input type="text" placeholder="Enter a Todo..." value={input} onChange={onInputChange} className="task-input" required />
       <button className="button-add" type="submit">
-        Add
+        {editTodo ? 'Ok' : 'Add'}
       </button>
     </form>
   );
